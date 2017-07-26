46d903eca48da61cbc73dd33fe32eebf9d582db4
paulstey <paulstey@gmail.com>
2017-07-26 16:04:47 -0400

    add a few usings

--- src/ClassImbalance.jl
+++ src/ClassImbalance.jl
@@ -3,6 +3,7 @@ module ClassImbalance
 using DataFrames
 using NullableArrays
 using CategoricalArrays     # NOTE: consider replacing these with pooled array
+using Distributions
 
 include("smote_exs.jl")
 include("ub_smote.jl")
