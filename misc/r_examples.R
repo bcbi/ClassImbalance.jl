d337fcfbf2fd0998860027544b783a7a62795fe8
paulstey <paulstey@gmail.com>
2017-08-01 15:35:29 -0400

    minor updates

--- misc/r_examples.R
+++ misc/r_examples.R
@@ -1,7 +1,10 @@
 library(DMwR)
 
-n <- 100
-betas <- c(1, 2, 3, 4, 1)
+n <- 1648
+pct_over <- 180
+pct_under <- 273
+n_minority <- round(0.1*n)
+n_majority <- round(0.9*n)
 
 # Create predictor variables
 X <- matrix(rnorm(n*10), ncol = 10)
@@ -9,10 +12,10 @@ X <- cbind(1, X)
 dat <- data.frame(X)
 
 # Creat outcome variable
-dat$y<- c(rep(1, 0.1*n), rep(0, 0.9*n))
+dat$y<- c(rep(0, n_majority), rep(1, n_minority))
 dat$y <- as.factor(dat$y)
 
 # Smote the data
-dat2 <- SMOTE(y ~ ., dat)
+dat2 <- SMOTE(y ~ ., dat, perc.over = pct_over, perc.under = pct_under)
 nrow(dat2)
 table(dat2$y)
