17bd5c8a5526c733306a52e6f55da3d22bbe1440
paulstey <paulstey@gmail.com>
2017-08-18 16:41:35 -0400

    change order of include

--- src/ClassImbalance.jl
+++ src/ClassImbalance.jl
@@ -5,10 +5,11 @@ using NullableArrays
 using CategoricalArrays     # NOTE: consider replacing these with pooled array
 using Distributions
 
+export smote, rose
+
 include("smote_exs.jl")
 include("ub_smote.jl")
 include("rose.jl")
 
-export smote, rose 
 
 end # module
