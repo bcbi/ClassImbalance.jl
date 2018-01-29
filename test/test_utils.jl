a5af971c5f13a90fdd73de7abfc54d0e1cf5555d
Dilum Aluthge <dilum@aluthge.com>
2018-01-29 13:53:24 -0500

    Create test_utils.jl

---
+++ test/test_utils.jl
@@ -0,0 +1,8 @@
+import DataFrames
+
+df = DataFrames.DataFrame()
+df[:a] = ["a", "b", "c"]
+df[:b] = [1, 2, 3]
+df[:c] = ["d", "e", "f"]
+df[:d] = [4.0, 5.0, 6.0]
+Base.Test.@test(ClassImbalance.factor_columns() == [2, 4])
