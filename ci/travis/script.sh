d7ab0c591e551fe80680b6922c46b24b738dd7a2
Dilum Aluthge <dilum@aluthge.com>
2019-05-19 01:21:15 -0400

    Update script.sh

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -4,37 +4,29 @@
 
 set -ev
 
-export COMPILED_MODULES=$COMP_MODS
-echo "COMPILED_MODULES=$COMPILED_MODULES"
-
-export JULIA_FLAGS="--check-bounds=yes --code-coverage=all --color=yes --compiled-modules=$COMPILED_MODULES --inline=no"
-echo "JULIA_FLAGS=$JULIA_FLAGS"
-
-export JULIA_PROJECT=@.
-
 cat Project.toml
 cat Manifest.toml
 
-julia $JULIA_FLAGS -e '
+julia --check-bounds=yes --color=yes -e '
     import Pkg;
     Pkg.build("ClassImbalance");
     '
 
-julia $JULIA_FLAGS -e '
+julia --check-bounds=yes --color=yes -e '
     import ClassImbalance;
     '
 
-julia $JULIA_FLAGS -e '
+julia --check-bounds=yes --color=yes -e '
     import Pkg;
     Pkg.test("ClassImbalance"; coverage=true);
     '
 
-julia $JULIA_FLAGS -e '
+julia --check-bounds=yes --color=yes -e '
     import Pkg;
     try Pkg.add("Coverage") catch end;
     '
 
-julia $JULIA_FLAGS -e '
+julia --check-bounds=yes --color=yes -e '
     import Coverage;
     import ClassImbalance;
     cd(normpath(joinpath(pathof(ClassImbalance), "..", "..")));
